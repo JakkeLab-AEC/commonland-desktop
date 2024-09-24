73cbb5360be424c0d1b3bd3b9dd086a1e5ab9b7f
JakkeLab <itsjakk3@gmail.com>
2024-09-25 00:42:02 +0900

    first commit

---
+++ src/rendererArea/screens/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <div className="flex flex-col">
+            <div>
+                Hi
+            </div>
+            <div className="mt-8">
+                This is
+            </div>
+        </div>
+    )
+}
